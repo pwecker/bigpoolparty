3f487de45802f920851162110ab8be4c06bbd2c3
paulwecker <paulwecker504@gmail.com>
2025-02-14 14:37:30 -0600

    map demo

--- front/public/lib/map.js
+++ front/public/lib/map.js
@@ -2,7 +2,22 @@ export function extendmap(Creative) {
 	return class extends Creative {
 		constructor() {
 			super();
-			console.log(L)
+
+			this.map = L.map('map',{
+				attributionControl: false,
+				maxZoom: 18,
+				zoomControl: false
+			});
+
+			L.control.zoom({
+			  position: 'bottomright'
+			})
+			.addTo(this.map);
+
+			L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {})
+			.addTo(this.map);
+
+			this.map.setView([40.776676, -73.971321], 13);
 		}
 	}
 }
