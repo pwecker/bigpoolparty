6e882f2e0d92bea203405b60b3c4e5c7b5f706e2
paulwecker <paulwecker504@gmail.com>
2025-02-13 20:14:39 -0600

    landing page

--- front/src/components/Land.vue
+++ front/src/components/Land.vue
@@ -1,8 +1,11 @@
 <template>
-	<div class="h-full w-full flex items-end justify-center p4"></div>
+	<div class="landing h-full w-full flex items-center justify-center p4"></div>
 </template>
 <script>
 export default {
 	name : ''
 }
 </script>
+<style scoped>
+  @import "@/components/Land.css"
+</style>
