cb5db1d81b80f1df3d5d7da09659d8bf5f8eaabb
paulwecker <paulwecker504@gmail.com>
2025-02-13 20:16:13 -0600

    demo page

--- front/src/components/Page.vue
+++ front/src/components/Page.vue
@@ -1,7 +1,14 @@
 <template>
+	<!-- <Device></Device> -->
+	<div class="h-full w-full flex flex-col gap-y-5 justify-center items-center">
+		<iframe width="300" height="250" src="/public/demo/map/index.html"></iframe>
+	  <iframe width="970" height="250" src="/public/demo/map/index.html"></iframe>
+  </div>
 </template>
 <script>
+import Device from '@/components/Device.vue';
 export default {
-	name : ''
+	name : 'Page',
+	components: { Device }
 }
 </script>
